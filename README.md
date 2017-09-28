f60a38aa68e9199f4fc044cf00a96d202d5e17e1
Alexandre B <contact@seldszar.fr>
2017-09-28 17:48:45 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+[![npm][npm]][npm-url]
+[![deps][deps]][deps-url]
+
+<div align="center">
+  <a href="https://github.com/webpack/webpack">
+    <img width="200" height="200" vspace="" hspace="25"
+      src="https://cdn.worldvectorlogo.com/logos/webpack-icon.svg">
+  </a>
+  <h1>Archiver Plugin</h1>
+  <p>Generate a compressed archive of compiled assets.<p>
+</div>
+
+## Install
+
+```bash
+npm install archiver-webpack-plugin --save-dev
+```
+
+## Usage
+
+```javascript
+var ArchiverPlugin = require("archiver-webpack-plugin");
+
+module.exports = {
+	plugins: [
+		new ArchiverPlugin({
+			format: "zip"
+		})
+	]
+}
+```
+
+Arguments:
+
+* `output`: The archive output path.
+* `test`: All assets matching this RegExp are processed. Defaults to every asset.
+* `format`: The archive format. Defaults to `"zip"`.
+* `formatOptions`: The archive format options. Defaults to `"{}"`.
+
+
+[npm]: https://img.shields.io/npm/v/archiver-webpack-plugin.svg
+[npm-url]: https://npmjs.com/package/archiver-webpack-plugin
+
+[deps]: https://david-dm.org/seldszar/archiver-webpack-plugin.svg
+[deps-url]: https://david-dm.org/seldszar/archiver-webpack-plugin
---
+++ src/index.js
@@ -0,0 +1,45 @@
+const archiver = require('archiver');
+const fs = require('fs');
+const url = require('url');
+
+class ArchiverWebpackPlugin {
+  constructor(options = {}) {
+    this.output = options.output;
+    this.test = options.test;
+    this.format = options.format || 'zip';
+    this.formatOptions = options.formatOptions || {};
+  }
+
+  apply(compiler) {
+    const ext = this.formatOptions.gzip ? `${this.format}.gz` : this.format;
+    const output = this.output || `${compiler.options.output.path}.${ext}`;
+
+    compiler.plugin('emit', (compilation, callback) => {
+      const { assets } = compilation;
+      const archive = archiver(this.format, this.formatOptions);
+
+      Object.keys(assets).forEach((file) => {
+        if (Array.isArray(this.test)) {
+          if (this.test.every(t => !t.test(file))) {
+            return;
+          }
+        } else if (this.test && !this.test.test(file)) {
+          return;
+        }
+
+        const asset = assets[file];
+        const content = asset.source();
+        const parse = url.parse(file);
+
+        archive.append(content, { name: parse.pathname });
+      });
+
+      archive.pipe(fs.createWriteStream(output));
+      archive.finalize();
+
+      callback();
+    });
+  }
+}
+
+export default ArchiverWebpackPlugin;
